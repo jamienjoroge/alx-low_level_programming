6979be0758de669205221d3c8b2441c1b839c962
jamienjoroge <jnjoroge141@gmail.com>
2022-09-13 11:57:08 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,12 +7,12 @@
  */
 int _isalpha(int c)
 {
-	if ((c > 64 && c < 122) || (c > 96 && c < 123))
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
-		}
+if ((c > 64 && c < 122) || (c > 96 && c < 123))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
