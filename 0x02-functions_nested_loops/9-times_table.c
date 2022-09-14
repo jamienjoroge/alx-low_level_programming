db6d559ff3cc3d3361919c932f0de98c0dcd6522
jamienjoroge <jnjoroge141@gmail.com>
2022-09-14 14:24:23 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,12 +9,12 @@ void times_table(void)
 	int x;
 	int y;
 
-	for ( x = 0; x < 10; x++)
+	for (x = 0; x < 10; x++)
 	{
 		for (y = 0; y < 10; y++)
 		{
 			_putchar((x * y) + '0');
-			_putchar('\n');
 		}
 	}
+	_putchar('n');
 }
