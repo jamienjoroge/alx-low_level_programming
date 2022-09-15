2e92bdc75ae1c63bab37bee9debd9dea30c14b5a
jamienjoroge <jnjoroge141@gmail.com>
2022-09-15 12:16:46 -0700

    multiplication

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,11 +8,9 @@ void print_numbers(void)
 {
 	int i;
 
-	for (i = 48; i < 58; i++)
+	for (i =0; i < 10; i++)
 	{
-		int j = i % 10;
-
-		_putchar('j');
+		_putchar('0' + i);
 	}
 	_putchar('\n');
 }
