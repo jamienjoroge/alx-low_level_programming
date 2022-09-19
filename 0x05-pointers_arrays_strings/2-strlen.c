8fa93ae5176685be73d5f86daa2de67b10724a39
jamienjoroge <jnjoroge141@gmail.com>
2022-09-19 05:38:39 -0700

    String length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_strlen -gets the length of a string
+ *@s: character to be detrmined
+ *Return: returns an int lentgh of string
+ */
+int _strlen(char *s)
+{
+	int size;
+
+	while (s[size] != '\0')
+	{
+		u++;
+	}
+	return (u);
+}
