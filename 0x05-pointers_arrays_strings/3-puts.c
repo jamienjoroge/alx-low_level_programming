09266a9c6f239e2f1c77a924209f0bd3a1806ef0
jamienjoroge <jnjoroge141@gmail.com>
2022-09-19 06:09:21 -0700

    print like puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_puts - prints string on screen
+ *@str: string to be printed
+ *Retrun:void
+ */
+void _puts(char *str)
+{
+	int i;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++
+	}
