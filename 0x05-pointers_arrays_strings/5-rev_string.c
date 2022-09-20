5ca1f05c82ffe10c952a02a5712f6abad43f92b6
jamienjoroge <jnjoroge141@gmail.com>
2022-09-20 11:25:01 -0700

    reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,17 +11,17 @@ void rev_string(char *s)
 	int size, i, half;
 	char temp;
 
-	for (len = 0; s[len] != '\0'; len++)
+	for (size = 0; s[size] != '\0'; len++)
 		;
 
 	i = 0;
 
-	half = len / 2;
+	half = size / 2;
 
 	while (half--)
 	{
-		temp = s[len - i - 1];
-		s[len - i - 1] = s[i];
+		temp = s[size - i - 1];
+		s[size - i - 1] = s[i];
 		s[i] = temp;
 		i++;
 }
