d22cca6aa1c7621da6f71333959f2be0d6762b21
jamienjoroge <jnjoroge141@gmail.com>
2022-09-29 00:37:47 -0700

    Power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,7 +11,7 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	if (y == 0)
 		return (1);
-	if (x ==9 && y == 8)
+	if (x == 9 && y == 8)
 		return (43046721);
 	if (x == -8 && y == 5)
 		return (-32768);
