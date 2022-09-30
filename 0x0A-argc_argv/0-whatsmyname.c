432ca2692e0a220ebb108d7359931798a8586a15
jamienjoroge <jnjoroge141@gmail.com>
2022-09-30 12:24:48 -0700

    Argc argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,10 @@
  * @argv: number
  * Return: 0
  */
-void main(int argc, char *argv[])
+void main(int argc, char* argv[])
 {
-	printf("%s\n", argv[0]);
+	if (argc != 0)
+		printf("%s\n", argv[0]);
+	else
+		printf("%d \n");
 }
